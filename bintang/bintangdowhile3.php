811bd82841eaf1900852e868e87907fe50bcd2c6
Herdy Si Bukan Anak Indie <93857692+herdyy69@users.noreply.github.com>
2022-01-28 14:45:26 +0700

    Add files via upload

---
+++ bintang/bintangdowhile3.php
@@ -0,0 +1,40 @@
+<?php
+error_reporting(E_ALL);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <table>
+            <tr>
+                <td><h4 style="font-family: Arial, Helvetica, sans-serif;">Jumlah bintang yang diinginkan</h4></td>
+                <td><h4><input type="text" name="bintang"></h4></td>
+                <td><h4><input type="submit" value="Proses"></h4></td>
+            </tr>
+        </table>
+</form>
+<h4 style="margin-top: -20px; margin-left: 5px;">
+    <?php
+        $v = $_POST['bintang'];
+        $i=1;
+        do{
+            $a=1;
+            do{
+                echo  $v-$a+1;
+                $a++;
+            }while($a <= $i);
+            echo "<br>";
+            $i++;
+        }while($i <= $v);
+    ?>
+</h4>
+</body>
+</html>
+
+
---
+++ bintang/bintangwhile1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <table>
+            <tr>
+                <td><h4 style="font-family: Arial, Helvetica, sans-serif;">Jumlah bintang yang diinginkan</h4></td>
+                <td><h4><input type="text" name="bintang"></h4></td>
+                <td><h4><input type="submit" value="Proses"></h4></td>
+            </tr>
+        </table>
+</form>
+<h4 style="margin-top: -20px; margin-left: 5px;">
+    <?php
+        $v = $_POST['bintang'];
+        $i=1;
+        while ($i <= $v){
+            $a=1;
+            while ($a <= $i){
+                echo  $a;
+                $a++;
+            }
+            $i++;
+            echo "<br>";
+        }
+    ?>
+</h4>
+</body>
+</html>
+
+
